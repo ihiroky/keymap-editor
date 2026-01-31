e8feda3859ba1265e10bf446f6155b8f47f5303b
Hiroki Itoh <ihiroky@gmail.com>
2026-01-31 23:30:09 +0900

    chore: Remove trailing shash for url

--- app/src/config.js
+++ app/src/config.js
@@ -6,8 +6,16 @@ function env(key) {
   return process.env[key] || process.env[`REACT_APP_${key}`]
 }
 
-export const apiBaseUrl = env('API_BASE_URL')
-export const appBaseUrl = env('APP_BASE_URL')
+function trimTrailingSlash(value) {
+  if (!value) {
+    return value
+  }
+
+  return value.replace(/\/+$/, '')
+}
+
+export const apiBaseUrl = trimTrailingSlash(env('API_BASE_URL'))
+export const appBaseUrl = trimTrailingSlash(env('APP_BASE_URL'))
 export const githubAppName = env('GITHUB_APP_NAME')
 export const enableGitHub = parseBoolean(env('ENABLE_GITHUB'))
 export const enableLocal = parseBoolean(env('ENABLE_LOCAL'))
